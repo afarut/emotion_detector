21405546162a30a6d113f1920de16e38d508f7f4
afarut <renat030205@ya.ru>
2024-04-21 12:51:25 +0300

    awdawd

--- main.py
+++ main.py
@@ -3,18 +3,36 @@
 
 from flask import Flask, jsonify, request, render_template
 import numpy as np
-
+import os
+from pathlib import Path
 
 app = Flask(__name__)
 
 
-
 # Cross Origin Resource Sharing (CORS) handling
 
-@app.route('/', methods=["GET"])
-def image_post_request():
-	print(request.json)
-	return render_template("index.html")
+
+@app.route('/upload', methods=["POST"])
+def upload_form():
+    data = request.form.to_dict()
+    return jsonify(data)
+
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    data = {}
+    if request.method == "POST":
+        tmp = request.form.to_dict()
+        data["partydate"] = tmp["partydate"]
+        data["title"] = tmp["title"]
+        title = f"{data['title']}datetime_{data['partydate']}".replace(":", "_")
+        file = request.files['file']
+        print(title)
+        path = "./static/before/" + title + "." + file.filename.split(".")[-1]
+        #file.save(str(path))
+        file.save(path)
+    return render_template("index.html", post=request.method == "POST", **data)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5555,debug=True)
